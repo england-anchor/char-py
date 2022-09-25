d9bc64736e8d8d0bcc9175226c505e89d0004b8e
england-anchor <england.anchor@gmail.com>
2022-09-25 06:56:40 -0500

    init

---
+++ char.py
@@ -0,0 +1,11 @@
+# Character lists
+uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+lowercase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphabet = uppercase + lowercase
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
+username_symbols = ['-', '_']
+password_symbols = ['!', '@', '#', '$', '%', '^', '-', '_', '+', '=', '?']
+
+# Whitelists
+username_whitelist = alphabet + numbers + username_symbols
+password_whitelist = alphabet + numbers
